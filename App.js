7a981aea48afb9fd40de512a74ae8246b3aa89e6
angel <angel@example.com>
2023-01-13 23:32:25 +0800

    add test background button

--- App.js
+++ App.js
@@ -2,37 +2,33 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import * as BackgroundFetch from 'expo-background-fetch';
 import * as TaskManager from 'expo-task-manager';
+import * as Location from 'expo-location';
 
 const BACKGROUND_FETCH_TASK = 'background-fetch';
-
+const LOCATION_TASK_NAME = 'background-location-task';
 // 1. Define the task by providing a name and the function that should be executed
 // Note: This needs to be called in the global scope (e.g outside of your React components)
 TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
   const now = Date.now();
-
   console.log(`Got background fetch call at date: ${new Date(now).toISOString()}`);
-
   // Be sure to return the successful result type!
+  await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+    accuracy: Location.Accuracy.Balanced,
+  });
   return BackgroundFetch.BackgroundFetchResult.NewData;
 });
 
-// 2. Register the task at some point in your app by providing the same name,
-// and some configuration options for how the background fetch should behave
-// Note: This does NOT need to be in the global scope and CAN be used in your React components!
-async function registerBackgroundFetchAsync() {
-  return BackgroundFetch.registerTaskAsync(BACKGROUND_FETCH_TASK, {
-    minimumInterval: 5, // 15 minutes
-    stopOnTerminate: false, // android only,
-    startOnBoot: true, // android only
-  });
-}
-
-// 3. (Optional) Unregister tasks by specifying the task name
-// This will cancel any future background fetch calls that match the given name
-// Note: This does NOT need to be in the global scope and CAN be used in your React components!
-async function unregisterBackgroundFetchAsync() {
-  return BackgroundFetch.unregisterTaskAsync(BACKGROUND_FETCH_TASK);
-}
+TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
+  if (error) {
+    // Error occurred - check `error.message` for more details.
+    return;
+  }
+  if (data) {
+    const { locations } = data;
+    // do something with the locations captured in the background
+    console.log(locations);
+  }
+});
 
 export default function App() {
   const [isRegistered, setIsRegistered] = React.useState(false);
@@ -58,7 +54,41 @@ export default function App() {
 
     checkStatusAsync();
   };
+  
+  // 2. Register the task at some point in your app by providing the same name,
+  // and some configuration options for how the background fetch should behave
+  // Note: This does NOT need to be in the global scope and CAN be used in your React components!
+  async function registerBackgroundFetchAsync() {
+    const { status: foregroundStatus } = await Location.requestForegroundPermissionsAsync();
+    if (foregroundStatus === 'granted') {
+      const { status: backgroundStatus } = await Location.requestBackgroundPermissionsAsync();
+      if (backgroundStatus === 'granted') {
+        return BackgroundFetch.registerTaskAsync(BACKGROUND_FETCH_TASK, {
+          minimumInterval: 5, // 15 minutes
+          stopOnTerminate: false, // android only,
+          startOnBoot: true, // android only
+        });
+      }
+    }
+  }
 
+  // 3. (Optional) Unregister tasks by specifying the task name
+  // This will cancel any future background fetch calls that match the given name
+  // Note: This does NOT need to be in the global scope and CAN be used in your React components!
+  async function unregisterBackgroundFetchAsync() {
+    return BackgroundFetch.unregisterTaskAsync(BACKGROUND_FETCH_TASK);
+  }
+  const requestPermissions = async () => {
+    const { status: foregroundStatus } = await Location.requestForegroundPermissionsAsync();
+    if (foregroundStatus === 'granted') {
+      const { status: backgroundStatus } = await Location.requestBackgroundPermissionsAsync();
+      if (backgroundStatus === 'granted') {
+        await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+          accuracy: Location.Accuracy.Balanced,
+        });
+      }
+    }
+  };
   return (
     <View style={styles.screen}>
       <View style={styles.textContainer}>
@@ -80,6 +110,7 @@ export default function App() {
         title={isRegistered ? 'Unregister BackgroundFetch task' : 'Register BackgroundFetch task'}
         onPress={toggleFetchTask}
       />
+      <Button onPress={requestPermissions} title="Enable background location" />
     </View>
   );
 }
